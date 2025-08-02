6a7ada72a9eb19e8056e7793ccb94ef3c1a90bc0
Spentine <114318049+Spentine@users.noreply.github.com>
2025-08-01 21:12:11 -0700

    parse html and get some parts of the song

--- fetcher/fetchLyrics.js
+++ fetcher/fetchLyrics.js
@@ -1,3 +1,5 @@
+import { DOMParser, Element } from "jsr:@b-fuze/deno-dom";
+
 const sitesData = {
   utaten: {
     name: "UtaTen",
@@ -20,15 +22,76 @@ const sitesData = {
       );
       
       // remove whitespace
-      link = link.replace(/\s+/g, ""); 
+      link = link.replace(/\s+/g, "");
       
-      console.log(link);
+      // fetch lyrics from the link
+      const response = await fetch(link);
+      const html = await response.text();
       
-      return [
-        {
-          lyrics: "insert lyrics here",
-        },
-      ];
+      const doc = new DOMParser().parseFromString(html, "text/html");
+      
+      // html table with results
+      const searchResultElement = (
+        doc.getElementsByClassName("searchResult")[0]
+          .getElementsByTagName("tbody")[0]
+      );
+      
+      const resultElements = [];
+      
+      // start at 1 to skip the header row
+      for (let i = 1; i < searchResultElement.children.length; i++) {
+        resultElements.push(
+          searchResultElement.children[i]
+        );
+      }
+      
+      const songResults = [];
+      
+      for (const resultElement of resultElements) {
+        const columns = resultElement.children;
+        
+        const linkElement = columns[0].getElementsByTagName("a")[0];
+        const songLink = linkElement.getAttribute("href");
+        const songTitle = linkElement.textContent.trim();
+        
+        const artistElement = columns[1].getElementsByTagName("p")[0];
+        const artists = [];
+        // first artist is a general artist and doesn't follow the same format
+        for (let i = 1; i < artistElement.children.length; i++) {
+          const element = artistElement.children[i];
+          const type = element.firstChild;
+          const typeMap = {
+            "作詞": "lyricist",
+            "作曲": "composer",
+            "編曲": "arranger",
+          };
+          // get the type by checking if the string includes the type
+          const typeKey = Object.keys(typeMap).find(key => type.textContent.includes(key));
+          
+          // if it doesn't match any type, idk the category so it will be ignored
+          if (!typeKey) continue;
+          
+          const name = element.children[0].textContent.trim();
+          artists.push({
+            name: name,
+            type: typeMap[typeKey],
+          });
+        }
+        
+        const lyricElement = columns[2].getElementsByTagName("a")[0];
+        const beginning = lyricElement.textContent.trim();
+        
+        const song = {
+          link: songLink,
+          title: songTitle,
+          artists: artists,
+          beginning: beginning,
+        };
+        
+        songResults.push(song);
+      }
+
+      return songResults;
     }
   }
 }
@@ -54,7 +117,7 @@ async function fetchLyrics(info) {
           results.push({
             site: site.name,
             url: site.url,
-            lyrics: song.lyrics,
+            songInfo: song,
           });
         }
       }
